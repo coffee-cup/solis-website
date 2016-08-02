f27bd3130f5d04373697caef0d0fadcfa7723873
Jake Runzer <jakerunzer@gmail.com>
2016-08-01 23:53:53 -0700

    update ga tracking code

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ import Styles from './scss/styles.scss';
 // Components
 import App from './components/App.jsx';
 
-ga.initialize('UA-77647623-1');
+ga.initialize('UA-81828512-1');
 
 function logPageView() {
     ga.pageview(window.location.pathname);
