97fe7491aa3f1e597085be5b8c0fbf9ff1b61c25
Jake Runzer <jakerunzer@gmail.com>
2018-06-22 11:50:02 -0700

    change footer link

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ class Footer extends Component {
             <footer className="grid-12-centered mt0 mb0">
                 <div className="col-6_md-8_sm-10_xs-12 pb0">
                     <p className="mt1 mb1 f4">
-                        Made with <span className="heart">&hearts;</span> by <a href='https://jakerunzer.xyz'>jake runzer</a>
+                        Made with <span className="heart">&hearts;</span> by <a href='https://jakerunzer.com'>jake runzer</a>
                     </p>
                 </div>
             </footer>
