fa6a8282a68d6d004740a4ac1fee68aa73500de2
Jake Runzer <jakerunzer@gmail.com>
2019-10-29 21:17:24 +0000

    remove ga

--- app/index.jsx
+++ app/index.jsx
@@ -4,21 +4,14 @@ import Styles from "./scss/styles.scss";
 import React from "react";
 import { render } from "react-dom";
 import { Router, Route, browserHistory } from "react-router";
-import ga from "react-ga";
 
 // Pages
 import App from "./components/App.jsx";
 import About from "./components/About.jsx";
 
-ga.initialize("UA-81828512-1");
-
-function logPageView() {
-  ga.pageview(window.location.pathname);
-}
-
 render(
-  <Router history={browserHistory} onUpdate={logPageView}>
+  <Router history={browserHistory}>
     <Route path="/" component={App} />
   </Router>,
-  document.getElementById("app")
+  document.getElementById("app"),
 );
