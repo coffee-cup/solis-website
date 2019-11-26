e1fb84b250096428566b363d5795d5bfb7fa16e1
Jake Runzer <jakerunzer@gmail.com>
2019-11-26 16:33:10 +0000

    add link to github in footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,7 +7,17 @@ class Footer extends Component {
         <div className="col-6_md-8_sm-10_xs-12 pb0">
           <p className="mt1 mb1 f4">
             Made with <span className="heart">&hearts;</span> by{" "}
-            <a href="https://jakerunzer.com">jake runzer</a>
+            <a href="https://jakerunzer.com">jake runzer</a>.
+            <br />
+            Source code available on{" "}
+            <a
+              href="https://github.com/coffee-cup/solis"
+              target="_blank"
+              rel="noopener"
+            >
+              Github
+            </a>
+            .
           </p>
         </div>
       </footer>
